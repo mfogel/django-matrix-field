550a0afa82e9ce58454a32b9cc919c80457d4192
Mike Fogel <mike@fogel.ca>
2012-12-23 21:52:15 -0800

    Initial set up tests

--- matrix_field/tests.py
+++ matrix_field/tests.py
@@ -1,20 +1,22 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.test import TestCase
 
 from . import MatrixField
 
-M1 = [3,4]
+
+M1 = ['a','b']
 M2 = [[3,4],[4,5]]
-M3 = [[[3,4,5,6],[12,13,14,15]],[[23,24,25,26],[33,34,35,36]]]
-M4 = [[[[2]]]]
+M3 = [[[1.3,1.4,1.5],[1.2,1.3,1.4]],[[2.3,2.4,2.5],[3.3,3.4,3.5]]]
+M4 = [[[[2.0]]]]
 
 
 class TestModel(models.Model):
-    mx = MatrixField()
-    mx_null = MatrixField(null=True)
-    mx_blank = MatrixField(blank=True)
-    mx_blank_null = MatrixField(blank=True, null=True)
+    str_2 = MatrixField(datatype=str, dimensions=(2,))
+    int_2x2 = MatrixField(datatype=int, dimensions=(2,2), blank=True)
+    float_2x2x3 = MatrixField(datatype=float, dimensions=(2,2,3), blank=True)
+    float_1x1x1x1 = MatrixField(datatype=float, dimensions=(1,1,1,1), blank=True)
 
 
 class TestModelForm(forms.ModelForm):
@@ -24,11 +26,75 @@ class TestModelForm(forms.ModelForm):
 
 class MatrixFieldModelFormTestCase(TestCase):
 
-    def test_valid1(self):
-        pass
+    def test_valid(self):
+        form = TestModelForm({
+            'str_2': M1,
+            'int_2x2': M2,
+            'float_2x2x3': M3,
+            'float_1x1x1x1': M4,
+        })
+        self.assertTrue(form.is_valid())
+        form.save()
+        self.assertEqual(TestModel.objects.count(), 1)
+
+    def test_invalid_type(self):
+        form = TestModelForm({
+            'str_2': [3.0, 4.0],
+        })
+        self.assertFalse(form.is_valid())
+
+    def test_invalid_dims1(self):
+        form = TestModelForm({
+            'float_1x1x1x1': [[1]],
+        })
+        self.assertFalse(form.is_valid())
+
+    def test_invalid_dims2(self):
+        form = TestModelForm({
+            'str_2': ['a','b','c'],
+        })
+        self.assertFalse(form.is_valid())
 
 
-class TimeZoneFieldDBTestCase(TestCase):
+class MatrixFieldDBTestCase(TestCase):
 
-    def test_valid1(self):
-        pass
+    def test_valid(self):
+        m = TestModel.objects.create(
+            str_2=M1,
+            int_2x2=M2,
+            float_2x2x3=M3,
+            float_1x1x1x1=M4,
+        )
+        m = TestModel.objects.get(pk=m.pk)
+        self.assertEqual(m.str_2, M1),
+        self.assertEqual(m.int_2x2, M2),
+        self.assertEqual(m.float_2x2x3, M3),
+        self.assertEqual(m.float_1x1x1x1, M4),
+
+    def test_valid_blank(self):
+        m = TestModel.objects.create(str_2=M1)
+        m = TestModel.objects.get(pk=m.pk)
+        self.assertEqual(m.int_2x2, None)
+        self.assertEqual(m.float_2x2x3, None)
+
+    def test_invalid_type(self):
+        with self.assertRaises(ValidationError):
+            TestModel(str_2=[2.0, 3.0]).full_clean()
+        with self.assertRaises(ValidationError):
+            TestModel(float_1x1x1x1=[[[[2]]]]).full_clean()
+
+    def test_invalid_dims(self):
+        #with self.assertRaises(ValidationError):
+        #    TestModel(str_2=['a']).full_clean()
+        with self.assertRaises(ValidationError):
+            TestModel(str_2=[['a'],['b']]).full_clean()
+        #with self.assertRaises(ValidationError):
+        #    TestModel(float_1x1x1x1=[[2.0]]).full_clean()
+
+    def test_invalid_altogether(self):
+        with self.assertRaises(ValidationError):
+            TestModel(str_2={'a': 2}).full_clean()
+        with self.assertRaises(ValidationError):
+            TestModel(str_2=4).full_clean()
+        with self.assertRaises(ValidationError):
+            TestModel(str_2='nil nil').full_clean()
